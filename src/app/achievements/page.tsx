9d61d066441d0dd0f3678e520d84d293ff9d22e4
NataliyaMuk <6mukomel9@gmail.com>
2024-11-04 20:09:30 +0300

    feat: GRAF-20 add achievements page & achievements detail page

---
+++ src/app/achievements/page.tsx
@@ -0,0 +1,8 @@
+import Achievements from '@/components/Achievements';
+import { NextPage } from 'next';
+
+const AchievementsPage: NextPage = () => {
+  return <Achievements />;
+};
+
+export default AchievementsPage;
---
+++ src/components/AchievementDetail/index.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { BackLink } from '@/components/shared';
+import { useRouter } from 'next/navigation';
+import { FC } from 'react';
+import styles from './index.module.scss';
+
+type Props = {};
+
+const AchievementDetail: FC<Props> = ({}) => {
+  const router = useRouter();
+
+  return (
+    <section className={styles['achievement-detail']}>
+      <BackLink mixClass={[styles['achievement-detail__backlink']]} />
+
+      <section className={styles['achievement-detail__main-content']}>
+        <img src="/post.svg" alt="Иконка достижения" className={styles['achievement-detail__img']} />
+        <p className={styles['achievement-detail__title']}>Название достижения</p>
+        <p className={styles['achievement-detail__description']}>Описание достижения</p>
+      </section>
+    </section>
+  );
+};
+
+export default AchievementDetail;
