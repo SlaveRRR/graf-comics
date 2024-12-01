4314106acbe38d91690c4ad16bdb17ed7510ab6e
artS3rg <art.serg.3002@mail.ru>
2024-12-01 23:08:43 +0300

    feat(web) GRAF-19: Новинки mobile

---
+++ src/components/Novelty/index.tsx
@@ -0,0 +1,42 @@
+'use client';
+import cn from 'classnames';
+import { FC } from 'react';
+import { Cards } from '../UI';
+import BackLink from '../shared/BackLink';
+import styles from './index.module.scss';
+
+const Novelty: FC = () => {
+  return (
+    <div className={styles['novetly__container']}>
+      <div className={'container'}>
+        <BackLink mixClass={[styles['novelty__backLink']]} />
+      </div>
+      <div className={styles['novelty__container']}>
+        <section className={cn(styles['novelty'], 'container')}>
+          <div className={styles['novetly__cards-container']}>
+            <Cards
+              mixClass={[styles['novetly__cards']]}
+              names={[
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+              ]}
+              isNews={[true, true, true, true, true, true, true, true, true, true, true, true]}
+            />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Novelty;
--- src/components/UI/Cards/index.tsx
+++ src/components/UI/Cards/index.tsx
@@ -1,21 +1,21 @@
-
-
-import React,{FC} from 'react'
-import { Card } from '..'
 import cn from 'classnames';
-import styles from './index.module.scss'
+import { FC } from 'react';
+import { Card } from '..';
+import styles from './index.module.scss';
 type Props = {
-    names:string[];
-    mixClass:string[];
-}
+  names: string[];
+  mixClass: string[];
+  isNews?: boolean[];
+};
 
-const Cards : FC<Props> = ({names,mixClass}) => {
+const Cards: FC<Props> = ({ names, mixClass, isNews = [] }) => {
   return (
-    <div className={cn(styles["cards"],...mixClass)}>
-       {names.map((v,i) => <Card key={`${v}${i}`} text={v} />)} 
+    <div className={cn(styles['cards'], ...mixClass)}>
+      {names.map((v, i) => (
+        <Card key={`${v}${i}`} text={v} isNew={isNews[i]} />
+      ))}
     </div>
-    
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
