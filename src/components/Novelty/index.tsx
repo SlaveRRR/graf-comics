12e4641a93191bd1b79ee454aaf514761e480441
artS3rg <art.serg.3002@mail.ru>
2024-12-01 14:05:23 +0300

    feat(web) GRAF-19: Новинки mobile

--- src/components/Novelty/index.tsx
+++ src/components/Novelty/index.tsx
@@ -7,29 +7,33 @@ import styles from './index.module.scss';
 
 const Novelty: FC = () => {
   return (
-    <div className={cn(styles['novetly__container'], 'container')}>
-      <BackLink mixClass={[styles['novelty__backLink']]} />
-      <section className={cn(styles['novelty'], 'container')}>
-        <div className={styles['novetly__cards-container']}>
-          <Cards
-            mixClass={[styles['novetly__cards']]}
-            names={[
-              'Название',
-              'Название',
-              'Название',
-              'Название',
-              'Название',
-              'Название',
-              'Название',
-              'Название',
-              'Название',
-              'Название',
-              'Название',
-              'Название',
-            ]}
-          />
-        </div>
-      </section>
+    <div className={styles['novetly__container']}>
+      <div className={'container'}>
+        <BackLink mixClass={[styles['novelty__backLink']]}/>
+      </div>
+      <div className={styles['novelty__container']}>
+        <section className={cn(styles['novelty'], 'container')}>
+          <div className={styles['novetly__cards-container']}>
+            <Cards
+              mixClass={[styles['novetly__cards']]}
+              names={[
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+                'Название',
+              ]}
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
