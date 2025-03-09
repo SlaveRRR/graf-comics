11c877bad6ae35a9aafdd0cb89375bdc08903872
SlaveRRR <slava.arhipov40020141@gmail.com>
2025-03-09 21:28:00 +0300

    feat: добавил страницы для ссылок в футере

---
+++ src/app/contract/page.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from 'next';
+
+const ContractPage: NextPage = () => {
+  return (
+    <div
+      className="container"
+      style={{
+        paddingTop: '120px',
+      }}
+    >
+      <h2
+        style={{ color: '#7A5AF8', fontWeight: '600', fontSize: '3.42rem', textAlign: 'center', marginBottom: '5px' }}
+      >
+        Ждет текст в фигме ;&#40;
+      </h2>
+    </div>
+  );
+};
+
+export default ContractPage;
---
+++ src/app/user-agreement/page.tsx
@@ -0,0 +1,8 @@
+import UserAgreement from '@/components/UserAgreement';
+import { NextPage } from 'next';
+
+const UserAgreementPage: NextPage = () => {
+  return <UserAgreement />;
+};
+
+export default UserAgreementPage;
