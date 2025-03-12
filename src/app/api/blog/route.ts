5c16dcb6534dcf86dd82b68180a56273dfec7cf9
SskamersS <newbiehoudini@yandex.ru>
2025-03-12 21:37:51 +0300

    feat: added GET request for blog

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const articles = await prisma.article.findMany();
+    return NextResponse.json(articles);
+  } catch (error) {
+    console.error('Ошибка при получении статей:', error);
+    return NextResponse.json({ error: 'Ошибка сервера' }, { status: 500 });
+  }
+}
