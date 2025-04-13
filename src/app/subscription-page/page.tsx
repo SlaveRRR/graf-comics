569a0adc8ec7b8ab74784d7b7c9b1ffd1581f8e3
ValentinaDorina <dorina_04@mail.ru>
2025-04-13 23:29:43 +0300

    feat: добавила модалку подписки

---
+++ src/app/subscription-page/page.tsx
@@ -0,0 +1,8 @@
+import ModalSubscription from '@/components/ModalSubscription';
+import { NextPage } from 'next';
+
+const ModalSubscriptionPage: NextPage = () => {
+  return <ModalSubscription />;
+};
+
+export default ModalSubscriptionPage;
---
+++ src/components/ModalSubscription/index.tsx
@@ -0,0 +1,24 @@
+import cn from 'classnames';
+import { FC } from 'react';
+import { Button } from '../UI';
+import styles from './index.module.scss';
+
+const ModalSubscription: FC = () => {
+  return (
+    <div className={styles['modalSubscription__container']}>
+      <h1 className={cn(styles['text--bold'], styles['text--margin-bottom-10'], styles['text--font-size-12'])}>
+        Уважаемый читатель
+      </h1>
+      <p className={cn(styles['text--grey'], styles['text--margin-bottom-40'], styles['text'])}>
+        Спасибо, что проявил интерес к нашему комиксу! Чтобы получить доступ к продолжению истории и наслаждаться новыми
+        главами без ограничений, вам потребуется оформить <span className={cn(styles['subscription'])}>подписку.</span>{' '}
+      </p>
+      <Button mixClass={[styles['btn']]}>Оформление подписки</Button>
+      <p className={cn(styles['text--lilac'], styles['text'])}>
+        Не упусти возможность погрузиться в увлекательный мир наших комиксов!
+      </p>
+    </div>
+  );
+};
+
+export default ModalSubscription;
