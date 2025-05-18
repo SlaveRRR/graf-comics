c0cf33da31a642ecf50f62e4bad39fd01b43e6dd
ArturFik <arturnike44@mail.ru>
2025-05-18 15:10:30 +0500

    fix: add bg & fix text

--- src/components/AchievementDetail/index.tsx
+++ src/components/AchievementDetail/index.tsx
@@ -16,8 +16,14 @@ const AchievementDetail: FC<Props> = ({}) => {
 
       <section className={styles['achievement-detail__main-content']}>
         <img src="/post.svg" alt="Иконка достижения" className={styles['achievement-detail__img']} />
-        <p className={styles['achievement-detail__title']}>Название достижения</p>
-        <p className={styles['achievement-detail__description']}>Описание достижения</p>
+        <div className={styles['achievement-detail--view']}>
+          <p className={styles['achievement-detail__title']}>
+            Название
+            <br />
+            &nbsp;достижения
+          </p>
+          <p className={styles['achievement-detail__description']}>Описание достижения</p>
+        </div>
       </section>
     </section>
   );
