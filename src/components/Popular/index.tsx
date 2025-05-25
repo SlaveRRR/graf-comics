de678b0b550c0246534d787d127c197df77dcb0c
ArturFik <arturnike44@mail.ru>
2025-05-25 13:34:45 +0500

    fix: fix ico page popular

--- src/components/Popular/index.tsx
+++ src/components/Popular/index.tsx
@@ -1,18 +1,34 @@
 'use client';
-import { FC } from 'react';
+import { useTheme } from '@/context/themeProvider';
+import { FC, useEffect, useState } from 'react';
+import PopularCards from '../PopularCards';
 import { BackLink } from '../shared';
 import styles from './index.module.scss';
-import PopularCards from '../PopularCards';
 const Popular: FC = () => {
+  const { theme, setTheme } = useTheme();
+  const [rating, setRating] = useState<string>();
+  const [vector, setVector] = useState<string>();
+  const [cup, setCup] = useState<string>();
+  useEffect(() => {
+    if (theme === 'light') {
+      setRating('/rating.svg');
+      setVector('/Vector.svg');
+      setCup('/cup.svg');
+    } else {
+      setRating('/ratingWhite.svg');
+      setVector('/VectorWhite.svg');
+      setCup('/cupWhite.svg');
+    }
+  }, [theme]);
   return (
     <div>
       <div className={'container'}>
-        <BackLink mixClass={[styles['popular__backLink']]}/>
+        <BackLink mixClass={[styles['popular__backLink']]} />
       </div>
       <div className={styles['container']}>
-        <PopularCards icon_text="/rating.svg" title_text="Топ рейтинга (месяц)"/>
-        <PopularCards icon_text="/Vector.svg" title_text="Топ просмотров (месяц)"/>
-        <PopularCards icon_text="/cup.svg" title_text="Лучшее за все время"/>
+        <PopularCards icon_text={rating} title_text="Топ рейтинга (месяц)" />
+        <PopularCards icon_text={vector} title_text="Топ просмотров (месяц)" />
+        <PopularCards icon_text={cup} title_text="Лучшее за все время" />
       </div>
     </div>
   );
